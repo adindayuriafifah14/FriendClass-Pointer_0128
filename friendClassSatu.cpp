9e745dd0cd3945a0ad7ca736fe4ffb15c4eda158
adindayuriafifah14 <adindaafifah14@gmail.com>
2026-04-28 19:04:20 +0700

    Menambahkan void siswa dan orang

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -22,3 +22,16 @@ public:
     void setId(int pId);
     void displayAll (orang &a);
 };
+
+void siswa::displayAll(orang &a)
+{
+    cout << id << endl << a.nama;
+}
+void orang ::setNama(string pNma)
+{
+    nama = pNma;
+}
+void siswa::setId(int pId)
+{
+    id = pId;
+}
